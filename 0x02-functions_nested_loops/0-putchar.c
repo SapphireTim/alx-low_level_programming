87706d05ab69582035ca7612a51b167c550506ad
SapphireTim <ooyekunle49@gmail.com>
2022-09-14 13:54:42 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,11 +5,7 @@
  * Return: always 0
  */
 
-<<<<<<< HEAD
 int main(void)
-=======
-int main(void);
->>>>>>> 45123e703085c6d1e813e1e29dd652024be4a47a
 {
 	_putchar('_');
 	_putchar('p');
@@ -19,11 +15,7 @@ int main(void);
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-<<<<<<< HEAD
 	_putchar('\n');
-=======
-	_putchar('\n')
->>>>>>> 45123e703085c6d1e813e1e29dd652024be4a47a
 
 	return (0);
 }
